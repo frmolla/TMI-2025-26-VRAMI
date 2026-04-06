57cf978afb2659147518456d7e357ae091924205
frmolla <frmolla@ucm.es>
2026-04-06 11:36:28 +0100

    Zoom in al añadir destinos y primeras rutas

--- Frontend/public/app/map/map.ts
+++ Frontend/public/app/map/map.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, OnDestroy, Input  } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, inject  } from '@angular/core';
 import mapboxgl, { Map } from 'mapbox-gl';
 import { IPoints } from './models/points.model';
 import { mapIcons } from './map-icons';
 import { Parada } from './models/parada.model';
 import { MapService } from '@/services/map.service';
 import { Subject, takeUntil } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-map',
@@ -18,6 +19,8 @@ mapStyle = 'mapbox://styles/mapbox/streets-v12'
 points: IPoints[] = [];
 @Input() ruta: Parada[] = [];
 markers: mapboxgl.Marker[] = [];
+
+private http = inject(HttpClient);
 
 private destroy$ = new Subject<void>();
 constructor(private mapService: MapService) {}
@@ -55,6 +58,7 @@ initMap() {
 
   this.map.on('load', () => {
     this.renderMarkers();
+    this.updateRoute();
   });
 
   // this.map.on('click', (e) => {
@@ -68,12 +72,25 @@ initMap() {
 addMarker(lugar: Parada){
     this.points.push({ nombre: lugar.nombre, coords: [lugar.lng, lugar.lat], status: 'active', orden: lugar.pos })
     console.log(`addMarker`)
+
+    this.map.flyTo({
+      center: [lugar.lng, lugar.lat],
+      zoom: 4,           // zoom al destino
+      speed: 1.2,         // velocidad de la animación
+      curve: 1.4,         // como se describe la curva
+      easing: (t) => t, 
+      essential: true
+    });
+    
+    this.updateRoute();
     this.renderMarkers()
 }
 
 eraseMarker(lugar: Parada){
     this.points = this.points.filter(p => p.nombre !== lugar.nombre);
     console.log(`eraseMarker`)
+    
+    this.updateRoute();
     this.renderMarkers()
 }
 
@@ -85,7 +102,8 @@ markerReorder() {
         orden: index + 1
     }));
 
-    this.renderMarkers();
+    this.updateRoute();
+    this.renderMarkers(); 
 }
 
 renderMarkers() {
@@ -107,6 +125,7 @@ renderMarkers() {
         const lngLat = marker.getLngLat();
         this.points[index].coords = [lngLat.lng, lngLat.lat];
         console.log(`drag`)
+        this.updateRoute();
       });
 
       this.markers.push(marker);
@@ -125,6 +144,56 @@ renderMarkers() {
 
     wrapper.appendChild(svgElement);
     return wrapper;
+  }
+
+  updateRoute() {
+    if (this.points.length < 2) {
+      if (this.map.getLayer('route-line')) {
+        this.map.removeLayer('route-line');
+      }
+      if (this.map.getSource('route')) {
+        this.map.removeSource('route');
+      }
+      return;
+    }
+
+    const coordsString = this.points.map((p) => p.coords.join(',')).join(';');
+
+    const directionsUrl = `https://api.mapbox.com/directions/v5/mapbox/driving/${coordsString}?geometries=geojson&access_token=${this.accessToken}`;
+
+    this.http.get<any>(directionsUrl).subscribe((response) => {
+      const routeGeoJSON = response.routes[0].geometry;
+
+      if (this.map.getLayer('route-line')) {
+        this.map.removeLayer('route-line');
+      }
+      if (this.map.getSource('route')) {
+        this.map.removeSource('route');
+      }
+
+      this.map.addSource('route', {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          properties: {},
+          geometry: routeGeoJSON,
+        },
+      });
+
+      this.map.addLayer({
+        id: 'route-line',
+        type: 'line',
+        source: 'route',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round',
+        },
+        paint: {
+          'line-color': '#3b9ddd',
+          'line-width': 5,
+        },
+      });
+    });
   }
 }
 
